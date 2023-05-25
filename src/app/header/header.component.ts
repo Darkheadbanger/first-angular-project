b7d59a57f2302cd359abd477fa7f303bfced64f5
David Bouhaben <davidbouhaben@yahoo.co.id>
2023-05-25 19:55:17 +0200

    animation work

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,27 +13,39 @@ import {
   styleUrls: ['./header.component.scss'],
   animations: [
     trigger('fadeInOut', [
-      state('in', style({ opacity: 0 })),
-      state('out', style({ opacity: 1 })),
-      transition('in <=> out', animate(1000)),
+      state('in', style({ opacity: 1})),
+      state('out', style({ opacity: 0})),
+      transition('in <=> out', animate(2000)),
     ]),
   ],
 })
 export class HeaderComponent implements OnInit {
   public stackTech: string[] = ['Full-stack', 'Front-end', 'Back-end'];
-  // On initialise l'indice `indiceStackTechAcctuel` à 0 pour commencer avec le premier élément du tableau.
   public indiceStackTechAcctuel: number = 0;
-  // On déclare `stackTechAcctuel` et on l'initialise avec le premier élément du tableau `stackTech`.
   public stackTechAcctuel: string = this.stackTech[this.indiceStackTechAcctuel];
+
   ngOnInit() {
-    setInterval(() => {
-      // Si l'indice atteint la fin du tableau `stackTech`, on le réinitialise à 0 pour recommencer avec le premier élément.
-      this.indiceStackTechAcctuel++;
-      if (this.indiceStackTechAcctuel >= this.stackTech.length) {
-        this.indiceStackTechAcctuel = 0;
-      }
-      // On met à jour `stackTechAcctuel` avec la technologie correspondant à l'indice actuel. Cela permet d'afficher la technologie actuelle dans le template HTML.      this.stackTechAcctuel = this.stackTech[this.indiceStackTechAcctuel];
-      this.stackTechAcctuel = this.stackTech[this.indiceStackTechAcctuel];
-    }, 1500);
+    this.cycleText();
+  }
+
+  cycleText() {
+    this.changeText().then(() => {
+      setTimeout(() => {
+        this.cycleText();
+      }, 2000);
+    });
+  }
+
+  changeText(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        this.indiceStackTechAcctuel++;
+        if (this.indiceStackTechAcctuel >= this.stackTech.length) {
+          this.indiceStackTechAcctuel = 0;
+        }
+        this.stackTechAcctuel = this.stackTech[this.indiceStackTechAcctuel];
+        resolve();
+      }, 2000);
+    });
   }
 }
