3584d2470bd4fbcd9ccb59d3dd059985bc11dba0
David Bouhaben <davidbouhaben@yahoo.co.id>
2023-06-04 13:54:39 +0200

    animation create

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -20,16 +20,29 @@ import {
   ],
 })
 export class HeaderComponent implements OnInit {
+  public name!: string;
+  public secondName!: string;
+  public lastName!: string;
+  public introdutionText!: string;
+  public profession!: string;
+
   public technologieStack: string[] = ['Full-Stack', 'Front-End', 'Back-End'];
   public indiceStackAcctuel: number = 0;
   public stackAcctuel: string = this.technologieStack[this.indiceStackAcctuel];
 
   ngOnInit() {
+    this.introdutionText = 'I am';
+    this.name = 'David';
+    this.secondName = 'Satria';
+    this.lastName = 'Bouhaben';
+    this.profession = 'Developper';
     // La function pour faire tourner l'animation
     this.changeText();
   }
 
-  // La fonction pour faire tourner l'application une fois la promesse est fait
+  // La fonction pour faire tourner l'application une fois la promesse est fait et va tourner
+  // En continue chaque 2 seconds car une fonctione recursive, changeText tourne, changeText fait appel
+  // a etapeAnimation, changeText rentre dans setTimeout et tourne pendant 2 seconds et c'est comme ca a l'infini
   changeText() {
     this.etapeAnimationText().then(() => {
       setTimeout(() => {
