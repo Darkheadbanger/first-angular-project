e74b044a1bee85a28afc55df33fe4e9c442fb7ae
David Bouhaben <davidbouhaben@yahoo.co.id>
2023-06-27 08:10:55 +0100

    try to use Observables to recieve multiples discogs API endpoints

--- src/app/discogsAPI.service.ts
+++ src/app/discogsAPI.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable} from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 import { MusicData } from './models/music-data.models';
 
 @Injectable({
@@ -9,10 +9,12 @@ import { MusicData } from './models/music-data.models';
 export class DiscogsAPI {
   constructor(private http: HttpClient) {}
   public title = 'lastFM-API';
+  public bigFour = [18845, 18839, 66025, 11770];
 
-  getAlbumInfo(): Observable<MusicData> {
-    return this.http.get<MusicData>(
-      'https://api.discogs.com/artists/18839'
-    );
+  getAlbumInfo(): void {
+    let request: Observable<MusicData>[] = this.bigFour.map((bigFours) => {
+      const urlApi = `https://api.discogs.com/artists/${bigFours}`;
+      return this.http.get<MusicData>(urlApi);
+    });
   }
 }
